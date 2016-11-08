812f5f562ac2fc826100d176abe0f83bd5117eeb
luodawei <1078926400@qq.com>
2016-11-08 22:50:35 +0800

    写了些跳转，发现其他页面连Activity都没写，就只跳了自己的

--- AndroidProject/MeiRenYu/app/src/main/java/meirenyu/SetLogin.java
+++ AndroidProject/MeiRenYu/app/src/main/java/meirenyu/SetLogin.java
@@ -1,7 +1,10 @@
 package meirenyu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.yss.yumeiren.R;
 
@@ -10,9 +13,31 @@ import com.yss.yumeiren.R;
  */
 
 public class SetLogin extends Activity {
+    TextView btnLogin;
+    TextView btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_login);
+        btnLogin = (TextView) findViewById(R.id.btn_login);
+        btnRegister = (TextView) findViewById(R.id.btn_register);
+        btnLogin.setOnClickListener(onClickListener);
+        btnRegister.setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_login:
+                    Intent intent = new Intent(SetLogin.this,HaveLoggedIn.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_register:
+                    Intent intent1 = new Intent(SetLogin.this,SetRegister.class);
+                    startActivity(intent1);
+                    break;
+            }
+        }
+    };
 }
